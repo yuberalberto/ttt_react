8e5ec93803dbff4c79bb405ed271f664b4d6c85a
yuberalberto <yuberalberto@gmail.com>
2021-09-12 14:49:56 -0500

    initial commit: the tictactoe game is functional

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ],
+    modules:[
+      'node_modules',
+      path.join(process.env.NPM_CONFIG_PREFIX || __dirname, 'lib/node_modules')
+    ]
+  },
+  resolveLoader: {
+    modules:[
+      'node_modules',
+      path.join(process.env.NPM_CONFIG_PREFIX || __dirname, 'lib/node_modules')
+    ]
+  },
+  devServer: {
+    port: 9950,
+    contentBase: './dist',
+    writeToDisk: true
+  }
+};
+
+module.exports = config;
